6f2331298b7b39a2a93ea25facae047001ca9717
aSHUrAI001 <ashurai.ar47@gmail.com>
2020-01-02 00:15:08 +0530

    first commit

---
+++ Data/DemoDBContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTAuthenticationWithcore.Data
+{
+    public class DemoDBContext : IdentityDbContext<IdentityUser>
+    {
+        public DemoDBContext(DbContextOptions<DemoDBContext> options) : base(options)
+        { }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            #region "Read seed Data"
+            builder.Entity<IdentityRole>().HasData(
+                new { Id = "1", Name = "Admin", NormalizedName = "ADMIN" },
+                new { Id = "2", Name = "Customer", NormalizedName = "CUSTOMER" }
+                );
+            #endregion
+
+        }
+    }
+}
